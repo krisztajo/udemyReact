73b6351dec3da6f166a8d9bc207f4b2bca673ef6
donlaiq <donlaiq@gmail.com>
2024-06-16 11:20:21 -0300

    Update Chart.js
    Changing variable name to toalValue.

--- code/06-finished/src/components/Chart/Chart.js
+++ code/06-finished/src/components/Chart/Chart.js
@@ -14,7 +14,7 @@ const Chart = (props) => {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={total}
+          totalValue={total}
           label={dataPoint.label}
         />
       ))}
